92eb7cf411b5316f66dca696d8c86cbed7e9f163
ivogp87 <ivaylopantaleev@hotmail.com>
2020-11-07 13:12:58 +0200

    Split VideoPlayer into 2 components

---
+++ client/src/components/VideoInfo/VideoInfo.js
@@ -0,0 +1,165 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faThumbsUp,
+  faThumbsDown,
+  faUser,
+  faComment,
+  faEye,
+  faCalendarWeek,
+} from '@fortawesome/free-solid-svg-icons';
+import './VideoInfo.css';
+import { formatNumber, formatDate } from '../../helpers';
+import { getVideoById } from '../../apis/youTube';
+
+const VideoInfo = ({ videoId }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [videoInfo, setVideoInfo] = useState({});
+  const [showFullDescription, setShowFullDescription] = useState(false);
+
+  // Fetch the video info from YouTube
+  useEffect(() => {
+    const fetchVideoInfo = async () => {
+      const videoData = await getVideoById(videoId);
+
+      if (videoData instanceof Error) {
+        setError(true);
+        setIsLoading(false);
+      } else {
+        setVideoInfo(videoData.items[0]);
+        setIsLoading(false);
+      }
+    };
+
+    fetchVideoInfo();
+  }, [videoId]);
+
+  // Render the video tags/keywords
+  const renderVideoTags = () => {
+    // Return null if there are no tags (empty array)
+    if (!videoInfo.snippet.tags.length) return null;
+
+    // Render only the first three tags
+    return videoInfo.snippet.tags.slice(0, 3).map((tag) => {
+      const urlEncodedTag = encodeURIComponent(tag).replace(/%20/gi, '+');
+      return (
+        <Link className="video-tag" key={tag} to={`/search?term=%23${urlEncodedTag}`}>
+          {tag}
+        </Link>
+      );
+    });
+  };
+
+  // Render the video description
+  const renderVideoDescription = () => {
+    // Empty description
+    if (!videoInfo.snippet.description.length) return null;
+
+    // Render the description
+
+    // Split the description on new line (\n)
+    const descriptionLines = videoInfo.snippet.description.split('\n');
+
+    // Show only 3 lines from the description by default
+    const itemsToShow = showFullDescription ? descriptionLines.length : 3;
+
+    // Show more/less button
+    const renderShowMoreLessButton = () => {
+      // Don't render the button if there are less than 3 items
+      if (descriptionLines.length <= 3) return null;
+
+      // render the button
+      const buttonText = showFullDescription ? 'Show Less' : 'Show More';
+
+      return (
+        <button
+          onMouseDown={(e) => {
+            e.preventDefault();
+            setShowFullDescription(!showFullDescription);
+          }}
+          className="btn show-description-btn"
+          type="button"
+        >
+          {buttonText}
+        </button>
+      );
+    };
+
+    return (
+      <div className="margin-y-2 video-description">
+        {descriptionLines.slice(0, itemsToShow).map((textLine, index) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <p className="description-text" key={index}>
+            {textLine}
+          </p>
+        ))}
+        {renderShowMoreLessButton()}
+      </div>
+    );
+  };
+
+  // Loading
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
+  // Error
+  if (error) {
+    return <div>Something went wrong. Please try again.</div>;
+  }
+
+  return (
+    <div className="video-info">
+      <div className="video-tags text-secondary">{renderVideoTags()}</div>
+      <h1 className="video-title">{videoInfo.snippet.title}</h1>
+      {/* Video stats - views, likes, etc */}
+      <div className="row video-stats">
+        <div className="row video-stats-main">
+          <p className="video-views">
+            <FontAwesomeIcon className="icon" icon={faEye} />
+            {formatNumber(Number(videoInfo.statistics.viewCount))}
+            &nbsp; views
+          </p>
+          <p className="publish-date">
+            <FontAwesomeIcon className="icon" icon={faCalendarWeek} />
+            {formatDate(new Date(videoInfo.snippet.publishedAt))}
+          </p>
+        </div>
+        <div className="row video-stats-secondary">
+          <p className="video-likes" title="Likes">
+            <FontAwesomeIcon className="icon" icon={faThumbsUp} />
+            {formatNumber(Number(videoInfo.statistics.likeCount))}
+          </p>
+          <p className="video-dislikes" title="Dislikes">
+            <FontAwesomeIcon className="icon" icon={faThumbsDown} />
+            {formatNumber(Number(videoInfo.statistics.dislikeCount))}
+          </p>
+          <p className="video-comments" title="Comments">
+            <FontAwesomeIcon className="icon" icon={faComment} />
+            {formatNumber(Number(videoInfo.statistics.commentCount))}
+          </p>
+        </div>
+      </div>
+      <a
+        className="margin-y-1 channel-title"
+        title={`Visit ${videoInfo.snippet.channelTitle}'s YouTube Channel`}
+        href={`https://www.youtube.com/channel/${videoInfo.snippet.channelId}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon className="icon" icon={faUser} />
+        {videoInfo.snippet.channelTitle}
+      </a>
+      {renderVideoDescription()}
+    </div>
+  );
+};
+
+VideoInfo.propTypes = {
+  videoId: PropTypes.string.isRequired,
+};
+
+export default VideoInfo;
--- client/src/components/VideoPlayer/VideoPlayer.js
+++ client/src/components/VideoPlayer/VideoPlayer.js
@@ -1,169 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faThumbsUp,
-  faThumbsDown,
-  faUser,
-  faComment,
-  faEye,
-  faCalendarWeek,
-} from '@fortawesome/free-solid-svg-icons';
-import { getVideoById } from '../../apis/youTube';
-import { formatNumber, formatDate } from '../../helpers';
 import './VideoPlayer.css';
 
 const VideoPlayer = ({ videoId, autoPlay }) => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(false);
-  const [videoInfo, setVideoInfo] = useState({});
-  const [showFullDescription, setShowFullDescription] = useState(false);
-
-  // Fetch the video info from YouTube
-  useEffect(() => {
-    const fetchVideoInfo = async () => {
-      const videoData = await getVideoById(videoId);
-
-      if (videoData instanceof Error) {
-        setError(true);
-        setIsLoading(false);
-      } else {
-        setVideoInfo(videoData.items[0]);
-        setIsLoading(false);
-      }
-    };
-
-    fetchVideoInfo();
-  }, [videoId]);
-
-  // Render the video tags/keywords
-  const renderVideoTags = () => {
-    // Return null if there are no tags (empty array)
-    if (!videoInfo.snippet.tags.length) return null;
-
-    // Render only the firs three tags
-    return videoInfo.snippet.tags.slice(0, 3).map((tag) => {
-      const urlEncodedTag = encodeURIComponent(tag).replace(/%20/gi, '+');
-      return (
-        <Link key={tag} to={`/search?term=%23${urlEncodedTag}`}>
-          {tag}
-        </Link>
-      );
-    });
-  };
-
-  // Render the video description
-  const renderVideoDescription = () => {
-    // Empty description
-    if (!videoInfo.snippet.description.length) return null;
-
-    // Render the description
-
-    // Split the description on new line (\n)
-    const descriptionLines = videoInfo.snippet.description.split('\n');
-
-    // Show only 3 lines from the description by default
-    const itemsToShow = showFullDescription ? descriptionLines.length : 3;
-
-    // Show more/less button
-    const renderShowMoreLessButton = () => {
-      // Don't render the button if there are less than 3 items
-      if (descriptionLines.length < 3) return null;
-
-      const buttonText = showFullDescription ? 'Show Less' : 'Show More';
-      // render the button
-      return (
-        <button
-          onMouseDown={(e) => {
-            e.preventDefault();
-            setShowFullDescription(!showFullDescription);
-          }}
-          className="btn"
-          type="button"
-        >
-          {buttonText}
-        </button>
-      );
-    };
-
-    return (
-      <div className="video-description padding-y-2">
-        {descriptionLines.slice(0, itemsToShow).map((textLine, index) => (
-          <p key={index}>{textLine}</p>
-        ))}
-        {renderShowMoreLessButton()}
-      </div>
-    );
-  };
-
-  // Loading
-  if (isLoading) {
-    return <div>Loading ...</div>;
-  }
-
-  // Error
-  if (error) {
-    return <div>Something went wrong. Please try again.</div>;
-  }
-
-  // Return the video player and info if everything's OK
   return (
     <div className="video-player">
-      <div className="video-iframe">
-        <iframe
-          title={videoInfo.snippet.title}
-          width="100%"
-          height="auto"
-          src={`https://www.youtube-nocookie.com/embed/${videoInfo.id}?autoplay=${autoPlay}`}
-          frameBorder="0"
-          autoPlay="1"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        />
-      </div>
-      <div className="video-info">
-        <div className="video-tags text-secondary">{renderVideoTags()}</div>
-        <h1 className="video-title">{videoInfo.snippet.title}</h1>
-        <div className="video-meta">
-          <div className="meta-text">
-            <span>
-              <FontAwesomeIcon icon={faEye} />
-              {formatNumber(Number(videoInfo.statistics.viewCount))}
-              &nbsp; views
-            </span>
-            <span>
-              <FontAwesomeIcon icon={faCalendarWeek} />
-              {formatDate(new Date(videoInfo.snippet.publishedAt))}
-            </span>
-          </div>
-          <div className="meta-stats">
-            <span title="Likes">
-              <FontAwesomeIcon icon={faThumbsUp} />
-              {formatNumber(Number(videoInfo.statistics.likeCount))}
-            </span>
-            <span title="Dislikes">
-              <FontAwesomeIcon icon={faThumbsDown} />
-              {formatNumber(Number(videoInfo.statistics.dislikeCount))}
-            </span>
-            <span title="Comments">
-              <FontAwesomeIcon icon={faComment} />
-              {formatNumber(Number(videoInfo.statistics.commentCount))}
-            </span>
-          </div>
-        </div>
-        <a
-          className="channel-byline"
-          title={`Visit ${videoInfo.snippet.channelTitle}'s YouTube Channel`}
-          href={`https://www.youtube.com/channel/${videoInfo.snippet.channelId}`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FontAwesomeIcon icon={faUser} />
-          {videoInfo.snippet.channelTitle}
-        </a>
-        {renderVideoDescription()}
-      </div>
+      <iframe
+        className="yt-iframe"
+        title="YouTube video"
+        width="100%"
+        height="auto"
+        src={`https://www.youtube-nocookie.com/embed/${videoId}?autoplay=${autoPlay}`}
+        frameBorder="0"
+        autoPlay="1"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      />
     </div>
   );
 };
