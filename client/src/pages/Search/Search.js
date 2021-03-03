64bddb32a1cfc5527a6aea13daf31f2f2a33e9dd
ivogp87 <ivaylopantaleev@hotmail.com>
2021-03-03 22:32:56 +0200

    Move data fetching to Search.js

--- client/src/pages/Search/Search.js
+++ client/src/pages/Search/Search.js
@@ -1,33 +1,58 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './Search.module.scss';
-import parseQueryString from '../../helpers/parseQueryString';
-import SearchResults from '../../components/SearchResults';
-import Container from '../../components/Container';
+import { useLocation } from 'react-router-dom';
+import qS from 'query-string';
+import usePromise from 'react-fetch-hook/usePromise';
+import { getVideos } from '../../apis/youTube/youTube';
+import VideoList from '../../components/VideoList';
+import Loader from '../../components/Loader';
+import ErrorMessage from '../../components/ErrorMessage';
 
-const Search = ({ location: { search } }) => {
-  const searchTerm = parseQueryString(search);
+const Search = () => {
+  const location = useLocation();
+  const queryString = qS.parse(location.search);
+  const searchTerm = queryString.term;
+  const { isLoading, data = { items: null }, error = {} } = usePromise(
+    () => getVideos(searchTerm),
+    [searchTerm]
+  );
 
-  // No query string in the url
   if (!searchTerm) {
-    return <div>Please enter search term</div>;
+    return (
+      <ErrorMessage position="center">
+        <p>Please use the search form to search for videos.</p>
+      </ErrorMessage>
+    );
+  }
+
+  if (error.message) {
+    const message =
+      error.message === 'Network Error'
+        ? 'Network Error. Please refresh the page and try again.'
+        : 'Something went wrong. Please try again.';
+    return (
+      <ErrorMessage position="center">
+        <p>{message}</p>
+      </ErrorMessage>
+    );
+  }
+
+  if (data.items && data.items.length === 0 && !isLoading) {
+    return (
+      <ErrorMessage position="center">
+        <p>No results found. Try with different keyword.</p>
+      </ErrorMessage>
+    );
+  }
+
+  if (isLoading && (!data.items || (data.items && data.items.length === 0))) {
+    return <Loader position="center" />;
   }
 
   return (
     <main>
-      <Container>
-        <div className={styles.searchResultsContainer}>
-          <SearchResults searchTerm={searchTerm} />
-        </div>
-      </Container>
+      <VideoList videos={data.items} showDescription showViews={false} />
     </main>
   );
-};
-
-Search.propTypes = {
-  location: PropTypes.shape({
-    search: PropTypes.string.isRequired,
-  }).isRequired,
 };
 
 export default Search;
