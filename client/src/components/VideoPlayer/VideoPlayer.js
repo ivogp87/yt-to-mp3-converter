55819874bbae4b306295018f874c96a1d921b13f
ivogp87 <ivaylopantaleev@hotmail.com>
2021-03-08 16:42:47 +0200

    Add new props to VideoPlayer

--- client/src/components/VideoPlayer/VideoPlayer.js
+++ client/src/components/VideoPlayer/VideoPlayer.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import styles from './VideoPlayer.module.scss';
 
-const VideoPlayer = ({ videoId, autoPlay }) => {
+const VideoPlayer = ({ videoId, videoTitle, autoPlay, className }) => {
   return (
-    <div className={styles.videoPlayer}>
+    <div className={classNames(styles.videoPlayer, className)}>
       <iframe
         className={styles.responsiveIframe}
-        title="YouTube video"
+        title={videoTitle || 'YouTube video'}
         width="100%"
         height="auto"
         src={`https://www.youtube-nocookie.com/embed/${videoId}?autoplay=${autoPlay}`}
@@ -22,11 +23,15 @@ const VideoPlayer = ({ videoId, autoPlay }) => {
 
 VideoPlayer.propTypes = {
   videoId: PropTypes.string.isRequired,
+  videoTitle: PropTypes.string,
   autoPlay: PropTypes.bool,
+  className: PropTypes.string,
 };
 
 VideoPlayer.defaultProps = {
   autoPlay: false,
+  videoTitle: null,
+  className: null,
 };
 
 export default VideoPlayer;
