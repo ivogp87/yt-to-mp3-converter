cfbb3efb7b5a344a9da2a18c862c3af0579ee624
ivogp87 <ivaylopantaleev@hotmail.com>
2021-03-08 16:44:41 +0200

    Move data fetching to Download.js page

--- client/src/pages/Download/Download.js
+++ client/src/pages/Download/Download.js
@@ -1,46 +1,123 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './Download.module.scss';
-import Container from '../../components/Container';
-import VideoPlayer from '../../components/VideoPlayer';
-import VideoInfo from '../../components/VideoInfo';
-import RelatedVideos from '../../components/RelatedVideos';
-import DownloadMp3 from '../../components/DownloadMp3';
+import React, { useState, useRef, useEffect } from 'react';
+import { saveAs } from 'file-saver';
+import { useParams } from 'react-router-dom';
+import usePromise from 'react-fetch-hook/usePromise';
+import { getRelatedVideos, getVideoDetails } from '../../apis/youTube/youTube';
+import downloadMp3 from '../../apis/backend/backend';
+import VideoContent from '../../components/VideoContent';
+import Loader from '../../components/Loader';
+import ErrorMessage from '../../components/ErrorMessage';
 
-const Download = ({
-  match: {
-    params: { videoId },
-  },
-}) => {
-  return (
-    <Container>
-      <div className={styles.wrapper}>
-        <main className={styles.mainContent}>
-          <div className={styles.player}>
-            <VideoPlayer videoId={videoId} autoPlay />
-          </div>
-          <div className={styles.download}>
-            <DownloadMp3 videoId={videoId} />
-          </div>
-          <div className={styles.videoInfo}>
-            <VideoInfo videoId={videoId} />
-          </div>
-        </main>
-        <aside className={styles.sidebar}>
-          <h3 className={styles.title}>Related Videos</h3>
-          <RelatedVideos videoId={videoId} />
-        </aside>
-      </div>
-    </Container>
+const Download = () => {
+  const { videoId } = useParams();
+
+  const {
+    isLoading: videoDetailsLoading,
+    data: videoDetails = { items: null },
+    error: videoDetailsError = {},
+  } = usePromise(() => getVideoDetails(videoId), [videoId]);
+
+  const { data: relatedVideos = { items: null }, error: relatedVideosError = {} } = usePromise(
+    () => getRelatedVideos(videoId),
+    [videoId]
   );
-};
 
-Download.propTypes = {
-  match: PropTypes.shape({
-    params: PropTypes.shape({
-      videoId: PropTypes.string.isRequired,
-    }),
-  }).isRequired,
+  const [downloadStatus, setDownloadStatus] = useState('idle');
+  const [isDownloadBtnDisabled, setIsDownloadBtnDisabled] = useState(false);
+
+  const isMounted = useRef(false);
+  useEffect(() => {
+    isMounted.current = true;
+
+    return () => {
+      isMounted.current = false;
+    };
+  });
+
+  const handleDownload = async () => {
+    setDownloadStatus('downloading');
+    setIsDownloadBtnDisabled(true);
+    try {
+      const { data, headers } = await downloadMp3(videoId);
+      const filename = headers['content-disposition'].split('=')[1].replace(/"/gi, '').trim();
+      saveAs(data, filename);
+
+      if (isMounted.current) setDownloadStatus('idle');
+    } catch (error) {
+      if (isMounted.current) setDownloadStatus('error');
+    } finally {
+      if (isMounted.current) setIsDownloadBtnDisabled(false);
+    }
+  };
+
+  useEffect(() => {
+    setIsDownloadBtnDisabled(false);
+  }, [videoId]);
+
+  if (videoDetailsError.message) {
+    const message =
+      videoDetailsError.message === 'Network Error'
+        ? 'Network Error. Please refresh the page and try again.'
+        : 'Something went wrong. Please try again.';
+    return (
+      <ErrorMessage position="center">
+        <p>{message}</p>
+      </ErrorMessage>
+    );
+  }
+
+  if (videoDetailsLoading && !videoDetails.items) return <Loader position="center" />;
+
+  if (videoDetails && videoDetails.items.length === 0) {
+    return (
+      <ErrorMessage position="center">
+        <p>Video not found.</p>
+      </ErrorMessage>
+    );
+  }
+
+  const {
+    id,
+    title,
+    channelTitle,
+    channelId,
+    publishedAt,
+    tags,
+    viewCount,
+    likeCount,
+    dislikeCount,
+    commentCount,
+    description,
+  } = videoDetails.items[0];
+
+  return (
+    <VideoContent
+      videoId={id}
+      autoPlay
+      videoTitle={title}
+      channelTitle={channelTitle}
+      channelId={channelId}
+      publishedAt={publishedAt}
+      tags={tags}
+      viewCount={viewCount}
+      likeCount={likeCount}
+      dislikeCount={dislikeCount}
+      commentCount={commentCount}
+      description={description}
+      relatedVideos={
+        videoDetails.items &&
+        videoDetails.items.length &&
+        relatedVideos.items &&
+        relatedVideos.items.length &&
+        !relatedVideosError.message
+          ? relatedVideos.items
+          : null
+      }
+      onDownload={handleDownload}
+      isDownloadDisabled={isDownloadBtnDisabled}
+      downloadStatus={downloadStatus}
+    />
+  );
 };
 
 export default Download;
