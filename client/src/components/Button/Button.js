a631d0a6bd8bfd2f6fcbc89634b3182f5d2b3c6c
ivogp87 <ivaylopantaleev@hotmail.com>
2021-03-08 16:39:48 +0200

    Change Button

--- client/src/components/Button/Button.js
+++ client/src/components/Button/Button.js
@@ -1,52 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames';
 import styles from './Button.module.scss';
 
-const Button = ({ icon, children, type, variant, color, size, extraClass, onClick }) => {
-  // Pass at least one of the two props!
-  if (children === '' && !icon) {
-    // eslint-disable-next-line no-console
-    console.warn('Pass string as children, icon as a prop or both to the button!');
-    return null;
-  }
-
-  // textPrimary, containedPrimary  // variant="text" color="primary"
-
-  const className = `${styles.btn} ${styles[`${variant}-${color}`]} ${styles[size]} ${extraClass}`;
-
+const Button = ({ onClick, type, icon, children, color, title, disabled, className }) => {
   return (
-    // eslint-disable-next-line react/button-has-type
-    <button onClick={onClick} className={className} type={type}>
-      {icon.iconName !== '' && <FontAwesomeIcon className={styles.icon} icon={icon} size="lg" />}
-      {children !== '' && <span>{children}</span>}
+    <button
+      onClick={onClick}
+      className={classNames(styles.button, styles[`button-${color}`], className)}
+      // eslint-disable-next-line react/button-has-type
+      type={type}
+      title={title}
+      disabled={disabled}
+    >
+      {icon && <FontAwesomeIcon className={styles.icon} icon={icon} size="lg" />}
+      {children}
     </button>
   );
 };
 
 Button.defaultProps = {
-  children: '',
-  type: 'button',
-  icon: { iconName: '', prefix: '' },
-  variant: 'contained',
-  color: 'primary',
-  size: 'medium',
-  extraClass: '',
   onClick: () => null,
+  icon: null,
+  title: null,
+  disabled: false,
+  className: null,
 };
 
 Button.propTypes = {
-  icon: PropTypes.shape({
-    iconName: PropTypes.string.isRequired,
-    prefix: PropTypes.string.isRequired,
-  }),
-  children: PropTypes.string,
-  type: PropTypes.oneOf(['button', 'submit']),
-  variant: PropTypes.oneOf(['text', 'contained']),
-  color: PropTypes.oneOf(['primary', 'secondary']),
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
-  extraClass: PropTypes.string,
   onClick: PropTypes.func,
+  type: PropTypes.oneOf(['button', 'submit', 'reset']).isRequired,
+  icon: PropTypes.string,
+  children: PropTypes.node.isRequired,
+  color: PropTypes.oneOf(['primary', 'secondary']).isRequired,
+  title: PropTypes.string,
+  disabled: PropTypes.bool,
+  className: PropTypes.string,
 };
 
 export default Button;
