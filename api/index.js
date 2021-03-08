3586a375862647cef511a6d4475b8d69db834c36
ivogp87 <ivaylopantaleev@hotmail.com>
2021-03-08 16:57:45 +0200

    Clean up - remove unneccessary comments

--- api/index.js
+++ api/index.js
@@ -6,12 +6,10 @@ const downloadRouter = require("./routes/download");
 const app = express();
 const PORT = 3001; // Don't use port 3000 - create react app uses port 3000
 
-// Allow CORS
 app.use(cors());
 
 app.use("/download", downloadRouter);
 
-// Start the server on port 3001
 app.listen(PORT, () => {
   console.log(`Server is live at port ${PORT}`);
 });
--- api/routes/download.js
+++ api/routes/download.js
@@ -6,35 +6,28 @@ const downloadRouter = express.Router();
 
 downloadRouter.get("/:videoId", async (req, res, next) => {
   try {
-    // YouTube Video url
     const videoUrl = `https://www.youtube.com/watch?v=${req.params.videoId}`;
 
-    // Validate the video id/url
     if (!ytdl.validateURL(videoUrl)) {
       return res.status(400).send({ error: "Invalid YouTube Url" });
     }
 
-    // Get the video info
     const videoInfo = await ytdl.getBasicInfo(videoUrl);
     const fileName =
       slugify(videoInfo.videoDetails.title, { replacement: " ", locale: "en", remove: /[\/\?<>\\:\*\|"]/g }) || "file";
 
-    // Set headers
     res.set({
       "Content-Disposition": `attachment; filename="${fileName}.mp3"`,
       "Access-Control-Expose-Headers": "Content-Disposition",
       "Content-Type": "audio/mpeg",
     });
 
-    // Download the audio file from YouTube
     const downloadAudio = ytdl(videoUrl, { quality: "highestaudio" });
 
-    // Handle download errors
     downloadAudio.on("error", (err) => {
       return res.status(400).send({ error: "Download failed!" });
     });
 
-    // Convert the file to MP3
     const convertAudio = new ffmpeg({ source: downloadAudio });
 
     // convert.setFfmpegPath("ffmpegLocation") // Uncomment only if you need to set the ffmpeg path here!
@@ -42,7 +35,6 @@ downloadRouter.get("/:videoId", async (req, res, next) => {
 
     convertAudio.withAudioCodec("libmp3lame").toFormat("mp3").output(res).run();
 
-    // Stop conversion and file download - usually invoked when the user cancels the download
     convertAudio.on("error", () => {
       convertAudio.kill();
       downloadAudio.destroy();
@@ -50,7 +42,6 @@ downloadRouter.get("/:videoId", async (req, res, next) => {
       return res.status(400).send({ error: "Download canceled by the user" });
     });
 
-    // The file is converted
     convertAudio.on("end", () => {
       return res.end();
     });
