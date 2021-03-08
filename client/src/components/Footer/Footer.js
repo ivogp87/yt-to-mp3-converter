8a1c6d1f2889d206d019642cb640c2dcca7c567c
ivogp87 <ivaylopantaleev@hotmail.com>
2021-03-08 17:00:20 +0200

    Change footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import { faYoutube, faFacebookF, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import styles from './Footer.module.scss';
 import Container from '../Container';
-import SocialMediaProfile from '../SocialMediaProfile';
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
       <Container>
-        <div className={styles.socialMediaProfiles}>
-          <SocialMediaProfile icon={faYoutube} link="https://youtube.com" color="secondary" />
-          <SocialMediaProfile icon={faTwitter} link="https://twitter.com" color="secondary" />
-          <SocialMediaProfile icon={faFacebookF} link="https://facebook.com" color="secondary" />
-          <SocialMediaProfile icon={faInstagram} link="https://instagram.com" color="secondary" />
-        </div>
+        <div className={styles.footerContent}>{new Date().getFullYear()}</div>
       </Container>
     </footer>
   );
