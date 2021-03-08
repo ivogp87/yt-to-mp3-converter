d9187df0ee1303447b6d83314189bca0f9e7b0ca
ivogp87 <ivaylopantaleev@hotmail.com>
2021-03-08 16:59:44 +0200

    Clean up - remove old unused components and utils

--- client/src/helpers/removeLiveVideos.js
+++
@@ -1,8 +0,0 @@
-const removeLiveVideos = (youTubeVideos) => {
-  const videos = youTubeVideos.filter(
-    (video) => video.snippet && video.snippet.liveBroadcastContent === 'none'
-  );
-  return videos;
-};
-
-export default removeLiveVideos;
